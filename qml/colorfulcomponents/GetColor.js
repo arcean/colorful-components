ba403f25b647e8f150156ba17ccbd22c567d9eb5
Tomasz Pieniążek <t.pieniazek@gazeta.pl>
2013-01-26 20:50:42 +0100

    Added GetColor.js.

---
+++ qml/colorfulcomponents/GetColor.js
@@ -0,0 +1,40 @@
+function getColor(color) {
+    if (color === "color2")
+        return "#61B500";
+    else if (color === "color3")
+        return "#3C870C";
+    else if (color === "color4")
+        return "#337608";
+    else if (color === "color5")
+        return "#2F6500";
+    else if (color === "color6")
+        return "#16ABCF";
+    else if (color === "color7")
+        return "#007CC7";
+    else if (color === "color8")
+        return "#0066BA";
+    else if (color === "color9")
+        return "#1C50AD";
+    else if (color === "color10")
+        return "#6300BA";
+    else if (color === "color11")
+        return "#850CB8";
+    else if (color === "color12")
+        return "#CA09B8";
+    else if (color === "color13")
+        return "#E5009F";
+    else if (color === "color14")
+        return "#EC5400";
+    else if (color === "color15")
+        return "#E7640A";
+    else if (color === "color16")
+        return "#F47119";
+    else if (color === "color17")
+        return "#FC8300";
+    else if (color === "color18")
+        return "#EA9307";
+    else if (color === "color19")
+        return "#EFAF11";
+
+    return "#1180DD";
+}
